f48a294c8974c61e021281e3bfd7f8dd0ff4305f
Qst236 <167086442+Qst236@users.noreply.github.com>
2024-08-20 00:11:35 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import os
+import re
 import json
 import pytz
 import time
+import random
 import discord
 import requests
 from discord import SyncWebhook
 from datetime import datetime, timedelta
+from scraper import start
+
+# news = [{'title': 'BusinessNZ Services Index', 'country': 'NZD', 'date': '2024-08-19T05:30:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '40.2'}, {'title': 'Rightmove HPI m/m', 'country': 'GBP', 'date': '2024-08-19T06:01:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '-0.4%'}, {'title': 'Core Machinery Orders m/m', 'country': 'JPY', 'date': '2024-08-19T06:50:00+07:00', 'impact': 'Low', 'forecast': '0.9%', 'previous': '-3.2%'}, {'title': 'FOMC Member Waller Speaks', 'country': 'USD', 'date': '2024-08-19T20:15:00+07:00', 'impact': 'Medium', 'forecast': '', 'previous': ''}, {'title': 'CB Leading Index m/m', 'country': 'USD', 'date': '2024-08-19T21:00:00+07:00', 'impact': 'Low', 'forecast': '-0.4%', 'previous': '-0.2%'}, {'title': 'Trade Balance', 'country': 'NZD', 'date': '2024-08-20T05:45:00+07:00', 'impact': 'Low', 'forecast': '331M', 'previous': '699M'}, {'title': '1-y Loan Prime Rate', 'country': 'CNY', 'date': '2024-08-20T08:15:00+07:00', 'impact': 'Medium', 'forecast': '3.35%', 'previous': '3.35%'}, {'title': '5-y Loan Prime Rate', 'country': 'CNY', 'date': '2024-08-20T08:15:00+07:00', 'impact': 'Medium', 'forecast': '3.85%', 'previous': '3.85%'}, {'title': 'Monetary Policy Meeting Minutes', 'country': 'AUD', 'date': '2024-08-20T08:30:00+07:00', 'impact': 'Medium', 'forecast': '', 'previous': ''}, {'title': 'Trade Balance', 'country': 'CHF', 'date': '2024-08-20T13:00:00+07:00', 'impact': 'Low', 'forecast': '5.44B', 'previous': '6.18B'}, {'title': 'German PPI m/m', 'country': 'EUR', 'date': '2024-08-20T13:00:00+07:00', 'impact': 'Low', 'forecast': '0.2%', 'previous': '0.2%'}, {'title': 'Foreign Direct Investment ytd/y', 'country': 'CNY', 'date': '2024-08-20T13:02:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '-29.1%'}, {'title': 'Current Account', 'country': 'EUR', 'date': '2024-08-20T15:00:00+07:00', 'impact': 'Low', 'forecast': '37.0B', 'previous': '36.7B'}, {'title': 'Final Core CPI y/y', 'country': 'EUR', 'date': '2024-08-20T16:00:00+07:00', 'impact': 'Low', 'forecast': '2.9%', 'previous': '2.9%'}, {'title': 'Final CPI y/y', 'country': 'EUR', 'date': '2024-08-20T16:00:00+07:00', 'impact': 'Low', 'forecast': '2.6%', 'previous': '2.6%'}, {'title': 'SNB Chairman Jordan Speaks', 'country': 'CHF', 'date': '2024-08-20T16:30:00+07:00', 'impact': 'Medium', 'forecast': '', 'previous': ''}, {'title': 'German Buba Monthly Report', 'country': 'EUR', 'date': '2024-08-20T17:03:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': ''}, {'title': 'CPI m/m', 'country': 'CAD', 'date': '2024-08-20T19:30:00+07:00', 'impact': 'High', 'forecast': '0.4%', 'previous': '-0.1%'}, {'title': 'Median CPI y/y', 'country': 'CAD', 'date': '2024-08-20T19:30:00+07:00', 'impact': 'High', 'forecast': '2.5%', 'previous': '2.6%'}, {'title': 'Trimmed CPI y/y', 'country': 'CAD', 'date': '2024-08-20T19:30:00+07:00', 'impact': 'High', 'forecast': '2.8%', 'previous': '2.9%'}, {'title': 'Common CPI y/y', 'country': 'CAD', 'date': '2024-08-20T19:30:00+07:00', 'impact': 'Medium', 'forecast': '2.2%', 'previous': '2.3%'}, {'title': 'Core CPI m/m', 'country': 'CAD', 'date': '2024-08-20T19:30:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '-0.1%'}, {'title': 'NHPI m/m', 'country': 'CAD', 'date': '2024-08-20T19:30:00+07:00', 'impact': 'Low', 'forecast': '0.0%', 'previous': '-0.2%'}, {'title': 'GDT Price Index', 'country': 'NZD', 'date': '2024-08-20T21:50:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '0.5%'}, {'title': 'FOMC Member Bostic Speaks', 'country': 'USD', 'date': '2024-08-21T00:35:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': ''}, {'title': 'FOMC Member Barr Speaks', 'country': 'USD', 'date': '2024-08-21T01:45:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': ''}, {'title': 'Trade Balance', 'country': 'JPY', 'date': '2024-08-21T06:50:00+07:00', 'impact': 'Low', 'forecast': '-0.73T', 'previous': '-0.82T'}, {'title': 'MI Leading Index m/m', 'country': 'AUD', 'date': '2024-08-21T07:30:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '0.0%'}, {'title': 'Credit Card Spending y/y', 'country': 'NZD', 'date': '2024-08-21T10:00:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '-3.1%'}, {'title': 'Public Sector Net Borrowing', 'country': 'GBP', 'date': '2024-08-21T13:00:00+07:00', 'impact': 'Low', 'forecast': '0.5B', 'previous': '13.6B'}, {'title': 'German 10-y Bond Auction', 'country': 'EUR', 'date': '2024-08-21T16:42:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '2.43|1.8'}, {'title': 'IPPI m/m', 'country': 'CAD', 'date': '2024-08-21T19:30:00+07:00', 'impact': 'Low', 'forecast': '-0.5%', 'previous': '0.0%'}, {'title': 'RMPI m/m', 'country': 'CAD', 'date': '2024-08-21T19:30:00+07:00', 'impact': 'Low', 'forecast': '-0.9%', 'previous': '-1.4%'}, {'title': 'Crude Oil Inventories', 'country': 'USD', 'date': '2024-08-21T21:30:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '1.4M'}, {'title': 'FOMC Meeting Minutes', 'country': 'USD', 'date': '2024-08-22T01:00:00+07:00', 'impact': 'High', 'forecast': '', 'previous': ''}, {'title': 'Flash Manufacturing PMI', 'country': 'AUD', 'date': '2024-08-22T06:00:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '47.4'}, {'title': 'Flash Services PMI', 'country': 'AUD', 'date': '2024-08-22T06:00:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '50.8'}, {'title': 'Flash Manufacturing PMI', 'country': 'JPY', 'date': '2024-08-22T07:30:00+07:00', 'impact': 'Low', 'forecast': '49.8', 'previous': '49.2'}, {'title': 'French Flash Manufacturing PMI', 'country': 'EUR', 'date': '2024-08-22T14:15:00+07:00', 'impact': 'High', 'forecast': '44.2', 'previous': '44.1'}, {'title': 'French Flash Services PMI', 'country': 'EUR', 'date': '2024-08-22T14:15:00+07:00', 'impact': 'High', 'forecast': '50.2', 'previous': '50.7'}, {'title': 'German Flash Manufacturing PMI', 'country': 'EUR', 'date': '2024-08-22T14:30:00+07:00', 'impact': 'High', 'forecast': '43.4', 'previous': '42.6'}, {'title': 'German Flash Services PMI', 'country': 'EUR', 'date': '2024-08-22T14:30:00+07:00', 'impact': 'High', 'forecast': '52.3', 'previous': '52.0'}, {'title': 'Flash Manufacturing PMI', 'country': 'EUR', 'date': '2024-08-22T15:00:00+07:00', 'impact': 'Medium', 'forecast': '45.7', 'previous': '45.6'}, {'title': 'Flash Services PMI', 'country': 'EUR', 'date': '2024-08-22T15:00:00+07:00', 'impact': 'Medium', 'forecast': '51.7', 'previous': '51.9'}, {'title': 'Flash Manufacturing PMI', 'country': 'GBP', 'date': '2024-08-22T15:30:00+07:00', 'impact': 'High', 'forecast': '52.1', 'previous': '51.8'}, {'title': 'Flash Services PMI', 'country': 'GBP', 'date': '2024-08-22T15:30:00+07:00', 'impact': 'High', 'forecast': '52.7', 'previous': '52.4'}, {'title': 'CBI Industrial Order Expectations', 'country': 'GBP', 'date': '2024-08-22T17:00:00+07:00', 'impact': 'Low', 'forecast': '-23', 'previous': '-32'}, {'title': 'ECB Monetary Policy Meeting Accounts', 'country': 'EUR', 'date': '2024-08-22T18:30:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': ''}, {'title': 'Unemployment Claims', 'country': 'USD', 'date': '2024-08-22T19:30:00+07:00', 'impact': 'High', 'forecast': '233K', 'previous': '227K'}, {'title': 'Flash Manufacturing PMI', 'country': 'USD', 'date': '2024-08-22T20:45:00+07:00', 'impact': 'High', 'forecast': '49.8', 'previous': '49.5'}, {'title': 'Flash Services PMI', 'country': 'USD', 'date': '2024-08-22T20:45:00+07:00', 'impact': 'High', 'forecast': '54.0', 'previous': '56.0'}, {'title': 'Consumer Confidence', 'country': 'EUR', 'date': '2024-08-22T21:00:00+07:00', 'impact': 'Low', 'forecast': '-13', 'previous': '-13'}, {'title': 'Existing Home Sales', 'country': 'USD', 'date': '2024-08-22T21:00:00+07:00', 'impact': 'Medium', 'forecast': '3.92M', 'previous': '3.89M'}, {'title': 'Natural Gas Storage', 'country': 'USD', 'date': '2024-08-22T21:30:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '-6B'}, {'title': 'Jackson Hole Symposium', 'country': 'All', 'date': '2024-08-22T23:15:00+07:00', 'impact': 'Medium', 'forecast': '', 'previous': ''}, {'title': 'Retail Sales q/q', 'country': 'NZD', 'date': '2024-08-23T05:45:00+07:00', 'impact': 'Medium', 'forecast': '-1.0%', 'previous': '0.5%'}, {'title': 'Core Retail Sales q/q', 'country': 'NZD', 'date': '2024-08-23T05:45:00+07:00', 'impact': 'Low', 'forecast': '-0.8%', 'previous': '0.4%'}, {'title': 'GfK Consumer Confidence', 'country': 'GBP', 'date': '2024-08-23T06:01:00+07:00', 'impact': 'Low', 'forecast': '-12', 'previous': '-13'}, {'title': 'National Core CPI y/y', 'country': 'JPY', 'date': '2024-08-23T06:30:00+07:00', 'impact': 'Low', 'forecast': '2.7%', 'previous': '2.6%'}, {'title': 'Core Retail Sales m/m', 'country': 'CAD', 'date': '2024-08-23T19:30:00+07:00', 'impact': 'High', 'forecast': '-0.4%', 'previous': '-1.3%'}, {'title': 'Retail Sales m/m', 'country': 'CAD', 'date': '2024-08-23T19:30:00+07:00', 'impact': 'High', 'forecast': '-0.3%', 'previous': '-0.8%'}, {'title': 'Corporate Profits q/q', 'country': 'CAD', 'date': '2024-08-23T19:30:00+07:00', 'impact': 'Low', 'forecast': '', 'previous': '0.6%'}, {'title': 'Fed Chair Powell Speaks', 'country': 'USD', 'date': '2024-08-23T21:00:00+07:00', 'impact': 'High', 'forecast': '', 'previous': ''}, {'title': 'New Home Sales', 'country': 'USD', 'date': '2024-08-23T21:00:00+07:00', 'impact': 'Medium', 'forecast': '628K', 'previous': '617K'}, {'title': 'Jackson Hole Symposium', 'country': 'All', 'date': '2024-08-23T23:15:00+07:00', 'impact': 'High', 'forecast': '', 'previous': ''}, {'title': 'BOE Gov Bailey Speaks', 'country': 'GBP', 'date': '2024-08-24T02:00:00+07:00', 'impact': 'High', 'forecast': '', 'previous': ''}, {'title': 'Jackson Hole Symposium', 'country': 'All', 'date': '2024-08-24T23:15:00+07:00', 'impact': 'Medium', 'forecast': '', 'previous': ''}]
 
 week = {
     'Mon' : 'Senin',
@@ -32,9 +37,9 @@ month = {
     'Dec' : 'Desember'
 }
 
-debug = False
+db = False
 def debug(text):
-    if debug:
+    if db:
         print(text)
 
 file_path = ".json"
@@ -54,16 +59,29 @@ def changeLang(date):
     d = date.replace(',', '').split(' ')
     return f"{week[ d[0] ]}, {d[1]} {month[ d[2] ]}"
 
+def userAgent():
+    user_agent_list = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 11.5; rv:90.0) Gecko/20100101 Firefox/90.0',
+        'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
+    ]
+    return random.choice(user_agent_list)
+
 def getNewsApi():
     try:
         url = "https://nfs.faireconomy.media/ff_calendar_thisweek.json"
         headers = {
-            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0"
+            "User-Agent": userAgent()
         }
 
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             data = response.json()
+
 
             for item in data:
                 item["date"] = changeTimezone(item["date"])
@@ -79,24 +97,51 @@ def changeTimezone(date):
     utc_plus_7 = datetime.fromisoformat(date).astimezone(pytz.timezone("Asia/Jakarta"))
     return utc_plus_7.isoformat()
 
+def createFlag(currency):
+    if currency == "All":
+        return "‎ " * 7
+    return f":flag_{currency[:2].lower()}:"
+
 def formatJsonData(data):
     data_by_date = {}
     last = ""
+
+    temp_data = start()
+    for x in range(len(data)):
+        data[x]["time"] = temp_data[x]["time"]
+
+    print(data)
+
     for item in data:
         date_str = item["date"][:10]
         date = datetime.strptime(date_str, "%Y-%m-%d")
         day = changeLang (date.strftime("%a, %d %b") )
-        time = (
-            "  All Day ` "
-            if item["impact"] == "Holiday"
-            else f"   {item['date'][11:16]}  ` "
-        )
+
         if day not in data_by_date:
             data_by_date[day] = []
     
-        data_by_date[day].append(
-            f":flag_{item['country'][:2].lower()}:  **{item['country']}**`{time}**{item['title']}**"
-        )
+        # if item["impact"] == "Holiday":
+        #     time = "  All Day "
+        # else:
+        #     time_str = item['date'][11:16]
+        #     timed = datetime.strptime(time_str, "%H:%M")
+        #     time = timed.strftime("%-H:%M").center(10, " ")
+
+        if re.search(":", item["time"]):
+            time_str = item['date'][11:16]
+            timed = datetime.strptime(time_str, "%H:%M")
+            time = timed.strftime("%-H:%M").center(10, " ")
+        else:
+            time = item["time"].center(10, " ")
+        
+        if item["impact"] != "Low" and item["impact"] != "Medium":
+            if time == last:
+                time = " " * 10
+            else:
+                last = time
+            data_by_date[day].append(
+                f"`{time}`{createFlag(item['country'])}  **{item['country'].upper()}** - **{item['title']}**"
+            )
         
      
 
@@ -134,7 +179,7 @@ def formatJsonData(data):
             content += f"{events}\n"
             
     weekly = []
-    if today.split(",")[0] == "Minggu":
+    if today.split(",")[0] == "Selasa":
         for day, events in data_by_date.items():
             weekly.append(f":date: **{day}**\n")
             weekly.extend(events)
@@ -144,23 +189,12 @@ def formatJsonData(data):
     debug(f"formatJsonData: {content, cweekly}")
     return content, cweekly
 
-def filterNews(data):
-    filtered_data = []
-    for item in data:
-        if item["impact"] != "Low" and item["impact"] != "Medium":
-            filtered_item = item.copy()
-            filtered_item["date"] = changeTimezone(filtered_item["date"])
-            filtered_data.append(filtered_item)
-
-    debug(f"filterNews: {filtered_data}")
-    return filtered_data
-
 def sendWebhook(daily, weekly, data):
     try:
         newData = {}
         webhook = SyncWebhook.from_url(
             os.environ["WEBHOOK_URL"]
-            # "https://discord.com/api/webhooks/1271664086563946536/c4AnCMnD2c_PNVgHVb3eW49IXNncw-mu9VFkIV-nTi1Y_PW9m_C8HuQdxweCwOsfCUWd"
+            # "https://discord.com/api/webhooks/1274771477941719103/P2dIS_YDdiCvDvATxM47CgfAdL6VKJbkQwokhQ_KU3-oD8TczmHMr9JiFMPIKHNZE5Xe"
         )
 
         if weekly:
@@ -197,7 +231,8 @@ def sendWebhook(daily, weekly, data):
 
 def main():
     data = read()
-    content = formatJsonData(filterNews(getNewsApi()))
+    content = formatJsonData(getNewsApi())
+    # content = formatJsonData(news)
     newData = sendWebhook(content[0], content[1], data)
     write(newData)
 
---
+++ scraper.py
@@ -0,0 +1,242 @@
+import re
+import time
+import json
+import pandas as pd
+from datetime import datetime
+import random
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service # Import the Service class
+
+ALLOWED_ELEMENT_TYPES = {
+    "calendar__cell": "date",
+    "calendar__cell calendar__date": "date",
+    "calendar__cell calendar__time": "time",
+    "calendar__cell calendar__currency": "currency",
+    "calendar__cell calendar__impact": "impact",
+    "calendar__cell calendar__event event": "event"
+}
+
+ICON_COLOR_MAP = {
+    "icon icon--ff-impact-yel": "yellow",
+    "icon icon--ff-impact-ora": "orange",
+    "icon icon--ff-impact-red": "red",
+    "icon icon--ff-impact-gra": "gray"
+}
+
+def contains_day_or_month(text):
+    """
+    Check if the given text contains a day of the week or a month.
+
+    Args:
+        text (str): The input text to check.
+
+    Returns:
+        tuple: A tuple containing a boolean indicating whether a match was found,
+        and the matched text (day or month) if found.
+    """
+
+     # Regular expressions for days of the week and months
+    days_of_week = r'\b(Mon|Tue|Wed|Thu|Fri|Sat|Sun)\b'
+    months = r'\b(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\b'
+    pattern = f'({days_of_week}|{months})'
+    
+    match = re.search(pattern, text, re.IGNORECASE)
+    
+    if not match:
+        return False,None
+    
+    matched_text = match.group(0)
+    if re.match(days_of_week, matched_text, re.IGNORECASE):
+        return True, matched_text
+  
+
+
+def find_pattern_category(text):
+    """
+    Find the category of a specific pattern within the given text.
+
+    Args:
+        text (str): The input text to analyze.
+
+    Returns:
+        tuple: A tuple containing a boolean indicating whether a match was found,
+        the category of the matched pattern, and the matched text.
+    """
+
+    # Regular expressions for different patterns
+    time_pattern = r'\d{1,2}:\d{2}(am|pm)'
+    day_pattern = r'Day\s+\d+'
+    date_range_pattern = r'\d{1,2}(st|nd|rd|th)\s*-\s*\d{1,2}(st|nd|rd|th)'
+    tentative_pattern = r'\bTentative\b'
+    pattern = f'({time_pattern}|{day_pattern}|{date_range_pattern}|{tentative_pattern})'
+    match = re.search(pattern, text, re.IGNORECASE)
+
+    if not match:
+        return False,None,None
+    
+    matched_text = match.group(0)
+    if re.match(time_pattern, matched_text, re.IGNORECASE):
+        category = "time"
+    elif re.match(day_pattern, matched_text, re.IGNORECASE):
+        category = "day_reference"
+    elif re.match(date_range_pattern, matched_text, re.IGNORECASE):
+        category = "date_range"
+    elif re.match(tentative_pattern, matched_text, re.IGNORECASE):
+        category = "tentative"
+    else:
+        category = "Unknown"
+    return True, category, matched_text
+
+def reformat_scraped_data(data):
+    """
+    Reformat scraped data and save it as a DataFrame and a CSV file.
+
+    Args:
+        data (list): The scraped data as a list of lists.
+        month (str): The month for naming the output CSV file.
+
+    Returns:
+        pd.DataFrame: The reformatted data as a DataFrame.
+    """
+    current_date = ''
+    current_time = ''
+    structured_rows = []
+
+    structured_json = []
+    
+
+    for row in data:
+        if len(row)==1 or len(row)==5:
+            match, day = contains_day_or_month(row[0])
+            if match:
+                current_date = row[0].replace(day,"").replace("\n","")
+        if len(row)==4:
+            current_time = row[0]
+
+        if len(row)==5:
+            current_time = row[1]
+        
+        if len(row)>1:
+            event = row[-1]
+            impact = row[-2]
+            currency = row[-3]
+            structured_rows.append([current_date, current_time, currency, impact, event])
+
+            data = {} # data for json
+            data["date"] = current_date
+            data["time"] = current_time
+            data["currency"] = currency
+            data["impact"] = impact
+            data["event"] = event
+
+            structured_json.append(data)
+
+                
+
+    # df = pd.DataFrame(structured_rows,columns=['date','time','currency','impact','event'])
+    # os.makedirs("news",exist_ok=True)
+    # df.to_csv(f"news/{month}_news.csv",index=False)
+
+    # write(structured_json)
+
+
+    # return df
+    return structured_json
+
+def create_driver():
+    user_agent_list = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 11.5; rv:90.0) Gecko/20100101 Firefox/90.0',
+        'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'
+    ]
+    user_agent = random.choice(user_agent_list)
+
+    browser_options = webdriver.ChromeOptions()
+    browser_options.add_argument("--no-sandbox")
+    browser_options.add_argument("--headless")
+    browser_options.add_argument("start-maximized")
+    browser_options.add_argument("window-size=1900,1080")
+    browser_options.add_argument("disable-gpu")
+    browser_options.add_argument("--disable-software-rasterizer")
+    browser_options.add_argument("--disable-dev-shm-usage")
+    browser_options.add_argument(f'user-agent={user_agent}')
+
+    # Create a Service object with the desired arguments
+    # service = Service(service_args=["--verbose", "--log-path=test.log"])
+
+    # Pass the Service object to the webdriver.Chrome constructor
+    # driver = webdriver.Chrome(options=browser_options, service=service)
+    driver = webdriver.Chrome(options=browser_options)
+
+    return driver
+
+def parse_data(driver, url):
+    driver.get(url)
+
+    table = driver.find_element(By.CLASS_NAME, "calendar__table")
+    data = []
+
+    # Scroll down to the end of the page
+    while True:
+        # Record the current scroll position
+        before_scroll = driver.execute_script("return window.pageYOffset;")
+        
+        # Scroll down a fixed amount
+        driver.execute_script("window.scrollTo(0, window.pageYOffset + 500);")
+        
+        # Wait for a short moment to allow content to load
+        time.sleep(2)
+        
+        # Record the new scroll position
+        after_scroll = driver.execute_script("return window.pageYOffset;")
+        
+        # If the scroll position hasn't changed, we've reached the end of the page
+        if before_scroll == after_scroll:
+            break
+    
+    # Now that we've scrolled to the end, collect the data
+    for row in table.find_elements(By.TAG_NAME, "tr"):
+        row_data = []
+        for element in row.find_elements(By.TAG_NAME, "td"):
+            class_name = element.get_attribute('class')
+            if class_name in ALLOWED_ELEMENT_TYPES:
+                if element.text:
+                    row_data.append(element.text)
+                elif "calendar__impact" in class_name:
+                    impact_elements = element.find_elements(By.TAG_NAME, "span")
+                    for impact in impact_elements:
+                        impact_class = impact.get_attribute("class")
+                        color = ICON_COLOR_MAP[impact_class]
+                    if color:
+                        row_data.append(color)
+                    else:
+                        row_data.append("impact")
+    
+        if len(row_data):
+            data.append(row_data)
+        
+
+    return data
+
+
+def start():
+    driver = create_driver()
+    url = 'https://www.forexfactory.com/calendar?week=this'
+
+    data = parse_data(driver, url)
+    # month =  datetime.now().strftime("%A")
+
+    ndata = reformat_scraped_data(data)
+    print(ndata)
+    
+
+    return ndata
+
+# start()
